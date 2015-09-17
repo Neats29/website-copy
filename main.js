394de2e41f9e6997b10c23046e7d6111e937de9c
Anita Amini <baadge29@yahoo.co.uk>
2015-09-17 17:41:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-//function navHover(x) {
-//	x.
-//}
+var io = require('socket.io').listen(9000);
+
+io.sockets.on('connection', function(socket) {
+  socket.on('message', function(message) {
+    io.sockets.emit('pageview', { 'url': message });
+  });
+});
