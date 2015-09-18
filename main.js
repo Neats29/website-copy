e04a05a6645d47d43fab51e636c8461e92f7f995
Anita Amini <baadge29@yahoo.co.uk>
2015-09-18 16:23:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var io = require('socket.io').listen(9000);
+var io = require('socket.io').listen(8000);
 
 io.sockets.on('connection', function(socket) {
   socket.on('message', function(message) {
